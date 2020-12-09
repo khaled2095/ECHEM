5ad777ac83b1587e52fa579c848e0b1af04ea614
khaled2095 <khaled_a.k@outlook.com>
2020-12-09 17:07:03 +0800

    Updated observer
    Added facebook image

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -17,7 +17,7 @@ class ProductObserver
             'mobileNo' => $phone,
             'message' => $msg,
         );
-    
+
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
@@ -27,11 +27,11 @@ class ProductObserver
         $output = curl_exec($curl);
         curl_close($curl);
     }
-    
-    
+
+
     public function created(Product $product){
         $prod = json_decode($product->product_attributes);
-            
+
             foreach($prod as $key => $i){
                 $x = new ProductAttribute();
                 $x->product_id = $product->id;
@@ -44,16 +44,17 @@ class ProductObserver
         SendTo::Facebook(
             'link',
             [
-                'link' => 'http://' . request()->getHttpHost() . '/product' . '/' . $product->id,
-                'message' => 'New product on our shop ' . $product->name
+                // 'link' => 'http://' . request()->getHttpHost() . '/product' . '/' . $product->id,
+                'message' => 'New product on our shop ' . $product->name . ' get @ ' . $product->price .'BDT' . ' http://' . request()->getHttpHost() . '/product' . '/' . $product->id,
+                'photo' => asset('storage/' . $product->cover_img),
             ]
         );
     }
-    
+
     public function updating(Product $product)
     {
         $prod = json_decode($product->product_attributes);
-            
+
             foreach($prod as $key => $i){
                 $x = new ProductAttribute();
                 $x->product_id = $product->id;
@@ -66,21 +67,21 @@ class ProductObserver
         if($product->isDirty('stock')){
             // email has changed
             if($product->stock < $product->min_qty){
-                
-                
+
+
                 if($product->shop_id){
                     $shop = DB::table('shops')->where('id', $product->shop_id)->first();
                     $shop_user = DB::table('users')->where('id', $shop->user_id)->first();
                     $this->sms('Your product with the name ' . $product->name. '  '.'low on stock. ' . 'Remaining stock is: '.$product->stock , $shop_user->phone);
                 }
-                
+
                 if($product->wholesale_id){
                     $shop = DB::table('wholesales')->where('id', $product->wholesale_id)->first();
                     $shop_user = DB::table('users')->where('id', $shop->user_id)->first();
                     $this->sms('Your product with the name ' . $product->name. '  '.'low on stock. ' . 'Remaining stock is: '.$product->stock, $shop_user->phone);
                 }
                 else{
-                    
+
                     $admin_phone = DB::table('users')->where('role_id', 1)->first();
                     $this->sms('Your product with the name ' . $product->name. '  '.'low on stock. ' . 'Remaining stock is: '.$product->stock, $admin_phone->phone);
                 }
